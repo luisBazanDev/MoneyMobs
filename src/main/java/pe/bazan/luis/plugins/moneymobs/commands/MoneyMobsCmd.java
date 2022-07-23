31c6a3e63580fe9deb66e00377bedb84cf2e2c99
Luis Bazán <luis@bazan.pe>
2022-07-23 11:40:20 -0500

    Modify command for users querry mobs info

--- src/main/java/pe/bazan/luis/plugins/moneymobs/commands/MoneyMobsCmd.java
+++ src/main/java/pe/bazan/luis/plugins/moneymobs/commands/MoneyMobsCmd.java
@@ -9,7 +9,29 @@ import java.util.concurrent.atomic.AtomicReference;
 
 public class MoneyMobsCmd {
   private MoneyMobs plugin;
-  private  CommandAPICommand info = new CommandAPICommand("info")
+  private  CommandAPICommand credits = new CommandAPICommand("credits")
+          .withPermission("moneymobs.command.moneymobs.credits")
+          .executesPlayer((sender, args) -> {
+            sender.sendMessage(format("&m====================&6&l Money Mobs &m===================="));
+            sender.sendMessage(format(String.format("&f&lVersion: %s", plugin.getDescription().getVersion())));
+            sender.sendMessage(format("&0&lGit&f&lHub&7: https://github.com/luisBazanDev/MoneyMobs"));
+            sender.sendMessage(format("&m===================================================="));
+          });
+
+  private  CommandAPICommand reload = new CommandAPICommand("reload")
+          .withPermission("moneymobs.command.moneymobs.reload")
+          .executesPlayer((sender, args) -> {
+            sender.sendMessage(format("&aReloading..."));
+            plugin.reloadConfig();
+            plugin.getMobsManager().registerMobs();
+            sender.sendMessage(format("&aReload successfully."));
+          });
+
+  private CommandAPICommand moneyMobs = new CommandAPICommand("moneymobs")
+          .withPermission("moneymobs.command.moneymobs")
+          .withAliases("moneym")
+          .withSubcommand(credits)
+          .withSubcommand(reload)
           .executesPlayer((sender, args) -> {
             sender.sendMessage(format("&m====================&6&l Money Mobs &m===================="));
             MobsManager.getMobs().forEach((k, v) -> {
@@ -20,29 +42,6 @@ public class MoneyMobsCmd {
                       v.getMax()
               )));
             });
-            sender.sendMessage(format("&m===================================================="));
-          });
-
-  private  CommandAPICommand reload = new CommandAPICommand("info")
-          .executesPlayer((sender, args) -> {
-            sender.sendMessage(format("&aReloading..."));
-            plugin.reloadConfig();
-            plugin.getMobsManager().registerMobs();
-            sender.sendMessage(format("&aReload successfully."));
-          });
-
-  private CommandAPICommand moneyMobs = new CommandAPICommand("moneymobs")
-          .withAliases("moneym")
-          .withPermission("moneymobs.command.moneymobs")
-          .withSubcommand(info)
-          .withSubcommand(reload)
-          .executesPlayer((sender, args) -> {
-            AtomicReference<String> loadedMobs = new AtomicReference<>("");
-            MobsManager.getMobs().forEach((k, v) -> loadedMobs.set(loadedMobs.get() + k.toString() + ";"));
-            sender.sendMessage(format("&m====================&6&l Money Mobs &m===================="));
-            sender.sendMessage(format("&f&l&nLoaded mobs:"));
-            sender.sendMessage(format(loadedMobs.get()));
-            sender.sendMessage(format("&0&lGit&f&lHub&7: https://github.com/luisBazanDev/MoneyMobs"));
             sender.sendMessage(format("&m===================================================="));
           });
 
